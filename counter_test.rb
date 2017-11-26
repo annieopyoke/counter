8157c4392565bbf9a1320b6d0eecb01deca38796
annieopyoke <annieopyoke@gmail.com>
2017-11-26 11:01:43 -0500

    array test added

--- counter_test.rb
+++ counter_test.rb
@@ -2,4 +2,8 @@ require "minitest/autorun"
 require_relative "counter.rb"
 class Testcounter < Minitest::Test
 
+def test_assert_that_counter_ar_is_an_array
+	assert_equal(Array, counterArClass())
+end
+
 end
