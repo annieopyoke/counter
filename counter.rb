6f2c56d2d32e04de8e0f38254420470b3a5efc26
annieopyoke <annieopyoke@gmail.com>
2017-11-26 13:51:52 -0500

    counter works

--- counter.rb
+++ counter.rb
@@ -1,5 +1,29 @@
-$counter = []
+$counterAr = []
 
 def counterArClass()
-	$counter.class 
+	$counterAr.class
+	end
+
+def counterClass(test_number)
+ 	test_number.class
+	end
+
+def counting(test_number)
+	counter = test_number
+	if counterClass = Integer
+	10.times do
+		$counterAr.push(counter)
+		counter += test_number
+	end
+puts $counterAr
+	if $counterAr.last % test_number == 0
+		true
+	else
+		false
+	end
+	else
+		false
+	end
 end
+ 
+  
--- counter_test.rb
+++ counter_test.rb
@@ -1,9 +1,22 @@
 require "minitest/autorun"
 require_relative "counter.rb"
-class Testcounter < Minitest::Test
-
+class TestCounter < Minitest::Test
+def test_assert_1_is_1
+	assert_equal(1, 1)
+	end
 def test_assert_that_counter_ar_is_an_array
 	assert_equal(Array, counterArClass())
+	end
+def test_assert_that_counter_is_an_integer
+	assert_equal(Integer, counterClass(1))
+	end
+def test_assert_that_counter_ar_contains_nums_divisible_by_5
+	assert_equal(true, counting(5))
+	end
+def test_assert_that_counter_ar_values_are_divisible_by_2
+	assert_equal(true, counting(2))
+	end
+def test_assert_that_wombat_is_rejected
+	assert_equal(false, counting("wombat"))
 end
-
 end
